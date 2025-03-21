e72a594458240c77d1e503e8b47956557ab7ec0d
Xiao Zheng <xiao.zheng@asurion.com>
2025-03-21 10:13:14 +0900

    fix source path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom"; // 添加HashRouter
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { useEffect } from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -36,6 +36,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <GitHubPagesRedirect />
       <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
